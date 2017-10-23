5b53e724cbb4651e10ca005a8fbec2370a3f4a6f
rlt3 <leroy@bythehilt.com>
2017-10-23 18:02:06 -0400

    Adding initial parsing of macro functions to the llexer.

--- src/lmacro.h
+++ src/lmacro.h
@@ -9,6 +9,17 @@
 static int llex (LexState *ls, SemInfo *seminfo);
 static int lmacro_llex (LexState *ls, SemInfo *seminfo);
 static void next (LexState *ls);
+static void inclinenumber (LexState *ls);
+static void esccheck (LexState *ls, int c, const char *msg);
+static void save (LexState *ls, int c);
+static const char *txtToken (LexState *ls, int token);
+
+#define skipwhitespace(ls) \
+    while (lisspace(ls->current)) { \
+        if (currIsNewline(ls)) \
+            inclinenumber(ls); \
+        next(ls); \
+    }
 
 /* Sometimes you don't want meta-recursion . . . */
 static inline int
@@ -84,6 +95,126 @@ lmacro_lua_setmacro (lua_State *L, const char *name, const char *definition)
     lua_pop(L, 1);
 }
 
+static int
+lmacro_tokenhasend (int t)
+{
+    switch (t) {
+        case TK_DO:
+        case TK_THEN:
+        case TK_FUNCTION:
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+static int
+lmacro_simpleform (const char *name, LexState *ls, SemInfo *seminfo)
+{
+    const char *def = NULL;
+
+    if (llex(ls, seminfo) != TK_STRING)
+        lexerror(ls, "Expected macro definition after name", 0);
+    def = getstr(seminfo->ts);
+
+    lmacro_lua_getmacrotable(ls->L);
+    lmacro_lua_setmacro(ls->L, name, def);
+
+    return next_lmacro(ls, seminfo);
+}
+
+
+/*
+ * simple macro functions are simply functions which accept an argument
+ * list that a basic reader macro function reads in from the input. Reading
+ * in input from Lua requires pushing a cfunction which has the current
+ * lexical scope in that cfunction scope somehow (either through C or 
+ * through a lua global value).
+ * When the ispartial returns a function and the current matching string 
+ * isn't in the reader macro list then that function is pushed to the 
+ * special reader function. That function simply looks for a form of 
+ * ([<anything except ','|,]+). It parses these arguments and passes them
+ * into the function passed to the reader.
+ */
+static int
+lmacro_functionform (const char *name, LexState *ls, SemInfo *seminfo)
+{
+    char buffer[BUFSIZ] = {0};
+    const char *str = NULL;
+    int i = 0;
+    int len = 0;
+    int parens = 0;
+    int ends = 1;
+    int t = ls->current;
+
+    len = strlen(name);
+    i = len;
+    strncpy(buffer, name, len);
+    buffer[i] = '\0';
+
+    while (parens > 0 || ends > 0) {
+        if (ls->current == EOZ || t == TK_EOS)
+            lexerror(ls, "Unexpected end of input in macro form", TK_EOS);
+
+        /* if `t' is a bonafide token instead of just a char */
+        if (t != ls->current) {
+            if (lmacro_tokenhasend(t))
+                ends++;
+            if (t == TK_END)
+                ends--;
+
+            /* TODO: possible to know long versus regular string by token? */
+            if (t == TK_STRING) {
+                buffer[i] = '[';
+                buffer[i+1] = '[';
+                buffer[i+2] = '\0';
+                i += 2;
+            }
+
+            switch (t) {
+                /* all the reserved keywords and symbols */
+                case TK_GOTO: case TK_IF: case TK_IN: case TK_LOCAL:
+                case TK_NIL: case TK_NOT: case TK_OR: case TK_REPEAT:
+                case TK_RETURN: case TK_THEN: case TK_TRUE: case TK_UNTIL:
+                case TK_WHILE: case TK_MACRO: case TK_IDIV: case TK_CONCAT:
+                case TK_DOTS: case TK_EQ: case TK_GE: case TK_LE:
+                case TK_NE: case TK_SHL: case TK_SHR: case TK_DBCOLON:
+                case TK_EOS:
+                    str = luaX_tokens[t - FIRST_RESERVED];
+                    break;
+                /* numbers, names */
+                default:
+                    str = getstr(seminfo->ts);
+                    break;
+            }
+
+            len = strlen(str);
+            strncat(buffer, str, len);
+            i += len;
+
+            if (t == TK_STRING) {
+                buffer[i] = ']';
+                buffer[i+1] = ']';
+                buffer[i+2] = '\0';
+                i += 2;
+            }
+        }
+        /* regardless of having a token, we always push the current char */
+        if (ls->current == '(')
+            parens++;
+        if (ls->current == ')')
+            parens--;
+
+        buffer[i] = ls->current;
+        buffer[i + 1] = '\0';
+        i++;
+
+        t = next_llex(ls, seminfo);
+    }
+    printf("%s\n", buffer);
+    return t;
+}
+
 /*
  * Parse a macro form. Right now this is simply the most basic form of:
  * macro <name> <string>
@@ -92,20 +223,17 @@ static int
 lmacro_define (LexState *ls, SemInfo *seminfo)
 {
     const char *name = NULL;
-    const char *def = NULL;
 
     if (next_llex(ls, seminfo) != TK_NAME)
         lexerror(ls, "Expected macro name in macro form", 0);
     name = getstr(seminfo->ts);
 
-    if (next_llex(ls, seminfo) != TK_STRING)
-        lexerror(ls, "Expected macro definition after name", 0);
-    def = getstr(seminfo->ts);
-
-    lmacro_lua_getmacrotable(ls->L);
-    lmacro_lua_setmacro(ls->L, name, def);
-
-    return next_lmacro(ls, seminfo);
+    skipwhitespace(ls);
+    
+    if (ls->current != '(')
+        return lmacro_simpleform(name, ls, seminfo);
+    else
+        return lmacro_functionform(name, ls, seminfo);
 }
 
 static int
